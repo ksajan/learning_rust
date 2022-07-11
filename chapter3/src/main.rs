7d41a1a7a35ec8eef73b25de30ffbdbac00fef8b
Sajan Kumar <ksajan12@gmail.com>
2022-07-12 00:47:45 +0530

    just started

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,3 +1,13 @@
+fn variables() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+}
+
 fn main() {
     println!("Common Programming Concepts!");
+    println!("============================");
+    println!("Topic One: Variables and Mutability");
+    variables();
 }
